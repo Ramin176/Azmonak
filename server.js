09f2377a59c49db3d7832c72bb3cd5a3ea0e04b1
Sayed Ramin Sadat <raminsadat205@gmail.com>
2025-09-01 13:24:47 +0430

    second commit

--- server.js
+++ server.js
@@ -41,7 +41,11 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server started on port ${PORT} and listening on all interfaces`);
+// });
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server started on port ${PORT} and listening on all interfaces`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
